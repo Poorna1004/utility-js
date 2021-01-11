e2dd5b02d495c6a69e29a12479930160cc2a3a44
Poorna V2 <pv2@lenovo.com>
2021-01-11 13:24:41 +0530

    Changed tail implementation and Fixed test

--- src/tail.js
+++ src/tail.js
@@ -1,7 +1,10 @@
-const tail = (array) => {
+const isArrayDefined = require('./isArrayDefined')
 
-    if(typeof array != "undefined" && array != null && array.length != 0)
-        return array[array.length - 1];
+const tail = (array) => {
+    arrayDefined = isArrayDefined(array)
+
+    if(arrayDefined)
+        return array.slice(1);;
 
     return undefined;
 }
---
+++ src/tail.spec.js
@@ -0,0 +1,29 @@
+const tail = require('./tail')
+
+describe('Tail', () => {
+
+    it('Tail of empty Array ([]) is undefined', () => {
+        expect(tail([])).toBeUndefined();
+    });
+
+    it('Tail of null is undefined', () => {
+        expect(tail(null)).toBeUndefined();
+    });
+
+    it('Tail of undefined is undefined', () => {
+       expect(tail(undefined)).toBeUndefined();
+    });
+
+    it('Tail of Array [1] is 1', () => {
+        expect(tail([1])).toEqual([])
+    });
+
+    it('Tail of Array [1,2,3] is [2,3]', () => {
+        expect(tail([1,2,3])).toEqual([2,3])
+    });
+
+    it('Tail of Array [3,4,5,-6] is [4,5,-6]', () => {
+        expect(tail([3,4,5,-6])).toEqual([4,5,-6])
+    });
+
+})
